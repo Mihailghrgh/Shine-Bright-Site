cead448486f6c3609fb86681c909e83329d615a8
Mihail <mihailghrgh@gmail.com>
2025-08-16 21:05:33 +0100

    Working9

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -14,51 +14,48 @@ import Footer from "./Footer";
 const Home = () => {
   return (
     <>
-      <div className="bg-white">
-        <Navbar isBordered className="bg-white/40 backdrop-blur-md">
-          <NavbarContent justify="center">
-            <NavbarItem>
-              <motion.h1
-                initial={{ opacity: 0, x: 50 }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ delay: 1, duration: 0.8 }}
-                className="text-blue-500 font-bold tracking-widest text-3xl md:text-3xl"
-              >
-                DACON
-              </motion.h1>
-              <motion.p
-                initial={{ opacity: 0, x: 50 }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ delay: 1, duration: 0.8 }}
-                className="text-blue-500 font-sans tracking-wide text-lg md:text-md"
-              >
-                SHINE BRIGHT
-              </motion.p>
-            </NavbarItem>
-          </NavbarContent>
-          <NavbarContent justify="end">
-            {navbarlinks.map((link) => {
-              return (
-                <NavbarItem>
-                  <div
-                    key={link.linkName}
-                    className="hidden items-center sm:w-full sm:h-full sm:flex sm:items-center "
+      <Navbar isBordered className="bg-white/40 backdrop-blur-md">
+        <NavbarContent className="bg-white" justify="center">
+          <NavbarItem>
+            <motion.h1
+              initial={{ opacity: 0, x: 50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ delay: 1, duration: 0.8 }}
+              className="text-blue-500 font-bold tracking-widest text-3xl md:text-3xl"
+            >
+              DACON
+            </motion.h1>
+            <motion.p
+              initial={{ opacity: 0, x: 50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ delay: 1, duration: 0.8 }}
+              className="text-blue-500 font-sans tracking-wide text-lg md:text-md"
+            >
+              SHINE BRIGHT
+            </motion.p>
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarContent className="bg-white" justify="end">
+          {navbarlinks.map((link) => {
+            return (
+              <NavbarItem>
+                <div
+                  key={link.linkName}
+                  className="hidden items-center sm:w-full sm:h-full sm:flex sm:items-center "
+                >
+                  <Link
+                    href={link.hrefLink}
+                    className={`${link.classType} font-bold`}
+                    type={link.typeLink}
                   >
-                    <Link
-                      href={link.hrefLink}
-                      className={`${link.classType} font-bold`}
-                      type={link.typeLink}
-                    >
-                      {link.linkName}
-                    </Link>
-                  </div>
-                </NavbarItem>
-              );
-            })}
-          </NavbarContent>
-        </Navbar>
-      </div>
-
+                    {link.linkName}
+                  </Link>
+                </div>
+              </NavbarItem>
+            );
+          })}
+        </NavbarContent>
+      </Navbar>
       <HomeTitle />
       <Servicii />
       <ClientCarousel />
