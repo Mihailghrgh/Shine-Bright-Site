95b96b8d2c407248f591a1398f6f4631a2d02687
Mihail <mihailghrgh@gmail.com>
2025-08-16 20:13:39 +0100

    Test6

--- components/Home/Footer.tsx
+++ components/Home/Footer.tsx
@@ -1,6 +1,9 @@
 function Footer() {
   return (
-    <footer id="footer" className="py-20 flex justify-center bg-gradient-to-r from-blue-400 via-blue-500 to-indigo-600">
+    <footer
+      id="footer"
+      className="py-20 flex justify-center bg-gradient-to-r from-blue-400 via-blue-500 to-indigo-600"
+    >
       <div className="flex space-x-0 md:space-x-56 flex-col md:flex-row justify-between items-center text-white">
         <div>
           <h1 className="font-bold tracking-wider text-6xl">DACON</h1>
@@ -8,11 +11,11 @@ function Footer() {
         </div>
 
         <div className="flex space-x-6 pt-4 text-white">
-          <p className="hover:text-background transition-colors">0735843957</p>
-          <p className="hover:text-background transition-colors">
+          <p className="hover:text-slate-50 transition-colors">0735843957</p>
+          <p className="hover:text-slate-50 transition-colors">
             dacon@gmail.com
           </p>
-          <p className="hover:text-background transition-colors">
+          <p className="hover:text-slate-50 transition-colors">
             București, Sector 6
           </p>
         </div>
