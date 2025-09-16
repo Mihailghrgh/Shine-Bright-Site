a6220d05858c53894749287f73e354a044145555
Mihail <mihailghrgh@gmail.com>
2025-09-16 13:43:13 +0100

    Resend fix

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -3,12 +3,11 @@ import { NextResponse } from "next/server";
 import { Resend } from "resend";
 import { emailTemplate } from "@/helpers/emailTemplate";
 
-
 export async function POST(req: Request) {
   const resend = new Resend(process.env.RESENT_API_KEY);
   const { email, message } = await req.json();
 
-  resend.emails.send({
+  await resend.emails.send({
     from: "onboarding@resend.dev",
     to: email,
     subject: "Mulțumim de email!",
